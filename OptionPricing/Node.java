f542522e5701b8b2ac1dd266cb8542de67929621
takanari-seito <t.seito.1110@gmail.com>
2015-11-13 10:54:38 +0900

    The node class for making graph in option pricing

---
+++ OptionPricing/Node.java
@@ -0,0 +1,23 @@
+
+public class Node {
+	//BinomialTreeを作るためのクラス
+
+	static int n;
+	static double u;
+	static double d;
+	static double p;
+	static double S;
+	public int i;
+	public int j;
+	public double X;
+	public double A;
+	public double P;
+	public double O;
+
+	//エッジはもう少し工夫すれば減らせそう
+	Node brother1,brother2;
+	Node child1,child2;
+
+	boolean check = false;
+
+}
